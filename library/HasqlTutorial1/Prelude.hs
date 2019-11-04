5f0f12cfedace72c4911dc76686d817bae755e6d
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-11-04 19:29:00 +0300

    Add docs on Prelude

--- library/HasqlTutorial1/Prelude.hs
+++ library/HasqlTutorial1/Prelude.hs
@@ -1,7 +1,16 @@
+{-|
+Project's internal prelude.
+We can reexport all things we want to be available in project's every module.
+-}
 module HasqlTutorial1.Prelude
 (
   module Exports,
 )
 where
 
+{-
+Please notice that this is not the prelude from "base",
+it is a richer drop-in replacement for it from the "rerebase" package.
+It provides us with a lot of useful types and functions like `Text` or `Vector`.
+-}
 import Prelude as Exports
